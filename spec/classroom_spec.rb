53242652f0b3f3196db94da4a1a68e2e1e5fcf0d
Ozzie <57572826+ozzman84@users.noreply.github.com>
2022-02-14 13:04:00 -0700

    Add test spec
    Create tests for first 2 iterations

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require './lib/classroom'
+
+describe Classroom do
+  context 'Attributes' do
+    it 'is a classroom' do
+      classroom = Classroom.new('History', 4)
+      expect(classroom).to be_an_instance_of(Classroom)
+    end
+
+    it 'has a subject' do
+      classroom = Classroom.new('History', 4)
+      expect(classroom.subject).to eq('History')
+    end
+
+    it 'has a capacity' do
+      classroom = Classroom.new('History', 4)
+      expect(classroom.capacity).to eq 4
+    end
+
+    it 'has no students by default' do
+      classroom = Classroom.new('History', 4)
+      expect(classroom.students).to eq []
+    end
+  end
+
+  # Iteration 2
+
+  context 'Students' do
+    it 'returns a list of students' do
+      classroom = Classroom.new('History', 4)
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+
+      expect(classroom.students).to eq ['Mike', 'Megan', 'Bob']
+    end
+
+    it 'returns a list of uppercased names' do
+      classroom = Classroom.new('History', 4)
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+
+      expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'BOB']
+    end
+  end
+end
